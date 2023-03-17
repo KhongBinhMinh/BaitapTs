693af59bb009a6a07c24748f9254a61b74aee574
Minhkb <minhkhongbavi@gmail.com>
2023-03-17 11:31:35 +0700

    baitap

--- practice/dist/practice.js
+++ practice/dist/practice.js
@@ -1,15 +1,21 @@
 var numArray4 = [4, 7, 3, 8, 9, 10];
 var numArray5 = ["Thi", "Hong", "Anh", "Nga"];
-function bubbleSort(arr) {
+function bubbleSort(arr, callback) {
     var _a;
     for (var i = 0; i < arr.length; i++) {
         for (var j = 0; j < arr.length; j++) {
-            if (arr[j] > arr[j + 1]) {
+            if ((arr[j] > arr[j + 1])) {
                 _a = [arr[j + 1], arr[j]], arr[j] = _a[0], arr[j + 1] = _a[1];
             }
         }
     }
     return arr;
 }
-bubbleSort(numArray5);
-console.log(numArray5);
+var result = bubbleSort(numArray4, function (item) {
+    return item;
+});
+console.log(result);
+var result2 = bubbleSort(numArray5, function (item) {
+    return item;
+});
+console.log(result2);
--- practice/src/practice.ts
+++ practice/src/practice.ts
@@ -1,19 +1,28 @@
-const numArray4 = [4,7,3,8,9,10]
-const numArray5 = ["Thi", "Hong", "Anh", "Nga"]
+const numArray4:number[] = [4,7,3,8,9,10]
+const numArray5:string[] = ["Thi", "Hong", "Anh", "Nga"]
 
 
-function bubbleSort<T>(arr: T[]): T[] {
+function bubbleSort<T>(arr: T[], callback: (item:T) => T): T[] {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
-            if (arr[j] > arr[j + 1]) {
+            if ( (arr[j] > arr[j + 1]) ) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];          
             }
         }
-    }
+    } 
 
     return arr;
 }
 
-bubbleSort(numArray5)
+const result = bubbleSort(numArray4, (item) => {
+   return item
+})
 
-console.log(numArray5);
+console.log(result);
+
+
+const result2 = bubbleSort(numArray5, (item) => {
+    return item
+ })
+ 
+ console.log(result2);
